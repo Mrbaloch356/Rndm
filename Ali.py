aab5a10fe2f05c53690a742cf25e4e4d0512a25f
Mrbaloch356 <102709281+Mrbaloch356@users.noreply.github.com>
2022-11-04 15:42:07 +0500

    Create Ali.py

---
+++ Ali.py
@@ -0,0 +1,18 @@
+import os,re,sys,platform
+
+os.system('git pull')
+
+os.system('pkg install play-audio')
+
+import requests
+
+ 
+
+bit = platform.architecture()[0]
+
+if bit == '64bit':
+
+    from cheek import ud
+
+    Main()
+
